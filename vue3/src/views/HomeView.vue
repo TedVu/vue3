67107b0bcbccce82553ac41f27e8ca9359322357
TedEngineer <tedvu184@gmail.com>
2023-05-15 21:50:24 +1000

    first bootstrap

---
+++ vue3/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="home">
+    <button class="btn">+</button>
+    <span class="counter"></span>
+    <button class="btn">-</button>
+  </div>
+</template>
+
+<style>
+.home {
+  text-align: center;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
