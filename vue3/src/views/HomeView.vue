0012ec11f1bac1f70d3e1f9900ca0fb17f7bc4d3
TedEngineer <tedvu184@gmail.com>
2023-05-15 21:54:53 +1000

    add a counter app

--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -2,11 +2,29 @@
 
 <template>
   <div class="home">
-    <button class="btn">+</button>
-    <span class="counter"></span>
-    <button class="btn">-</button>
+    <button class="btn" @click="decreaseCounter">-</button>
+    <span class="counter"> {{ counter }}</span>
+
+    <button class="btn" @click="increaseCounter">+</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
 
 <style>
 .home {
