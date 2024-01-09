a3d514d8a77ef7ea7050a29ed742da1a48293b0e
TedEngineer <tedvu184@gmail.com>
2024-01-09 22:08:05 +1100

    experiment model and synching values

---
+++ vue3/src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ modelValue }}</div>
+</template>
+
+<script setup lang="ts">
+import { watch } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  }
+})
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    console.log(`New value is ${newValue}`)
+  }
+)
+</script>
--- vue3/src/views/HomeView.vue
+++ vue3/src/views/HomeView.vue
@@ -1,35 +1,20 @@
-<script setup></script>
-
 <template>
-  <h3>{{ counterData.title }}</h3>
-  <div class="home">
-    <button class="btn" @click="decreaseCounter">-</button>
-    <span class="counter"> {{ counterData.count }}</span>
-
-    <button class="btn" @click="increaseCounter(1)">+</button>
-    <button class="btn" @click="increaseCounter(2)">++</button>
-  </div>
-  <div class="edit">
-    <h4>Edit counter title:</h4>
-    <input v-model="counterData.title" type="text" />
+  <div>
+    <ChildComponent
+      :modelValue="counterData.title"
+      @update:modelValue="counterData.title = $event"
+    />
+    <input v-model="counterData.title" />
   </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
+import ChildComponent from '../components/ChildComponent.vue'
 
 const counterData = reactive({
-  count: 0,
   title: 'My Counter'
 })
-
-const increaseCounter = (amount) => {
-  counterData.count += amount
-}
-
-const decreaseCounter = () => {
-  counterData.count--
-}
 </script>
 
 <style>
